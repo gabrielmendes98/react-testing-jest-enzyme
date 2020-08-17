94af05aa68eb92dfd73a3705d355e07b5fb22ce7
Gabriel <gabrielmssantiago@gmail.com>
2020-08-17 16:15:16 -0300

    state update on change and clears on click implementation and test

--- jotto/src/components/Input/index.test.tsx
+++ jotto/src/components/Input/index.test.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { shallow } from 'enzyme';
+import React, { useState } from 'react';
+import { shallow, ShallowWrapper } from 'enzyme';
 
 import { findByTestAttr } from '../../../tests/testUtils';
 import Input from './index';
@@ -10,4 +10,30 @@ test('input renders without error', () => {
   const wrapper = setup();
   const component = findByTestAttr(wrapper, 'component-input');
   expect(component.length).toBe(1);
+});
+
+describe('state controlled input field', () => {
+  const mockSetCurrentGuess = jest.fn();
+  let wrapper: ShallowWrapper;
+
+  beforeEach(() => {
+    mockSetCurrentGuess.mockClear();
+    React.useState = jest.fn(() => ['', mockSetCurrentGuess]);
+
+    wrapper = setup();
+  });
+  test('state updates with value of input box upon change', () => {
+    const inputBox = findByTestAttr(wrapper, 'input-box');
+
+    const mockEvent = { target: { value: 'train' } };
+    inputBox.simulate('change', mockEvent);
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith('train');
+  });
+  test('field is cleared upon submit button click', () => {
+    const submitButton = findByTestAttr(wrapper, 'submit-button');
+
+    submitButton.simulate('click', { preventDefault() {} });
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith('');
+  });
 });
--- jotto/src/components/Input/index.tsx
+++ jotto/src/components/Input/index.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 interface Props {
   secretWord: string;
 }
 
 const Input: React.FC<Props> = ({ secretWord }) => {
-  return <div data-test="component-input"></div>;
+  const [currentGuess, setCurrentGuess] = React.useState('');
+
+  function handleSubmit(e: React.MouseEvent) {
+    e.preventDefault();
+    setCurrentGuess('');
+  }
+
+  return (
+    <div data-test="component-input">
+      <form>
+        <input
+          data-test="input-box"
+          placeholder="enter guess"
+          type="text"
+          value={currentGuess}
+          onChange={(e) => setCurrentGuess(e.target.value)}
+        />
+        <button data-test="submit-button" onClick={handleSubmit}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Input;
