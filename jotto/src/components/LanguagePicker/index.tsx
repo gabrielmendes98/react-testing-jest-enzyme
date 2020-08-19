ecf842d2731ed2f162c1e298d52db25fb6378469
Gabriel <gabrielmssantiago@gmail.com>
2020-08-19 18:28:58 -0300

    setlanguage implementation and test

---
+++ jotto/src/components/LanguagePicker/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+interface Props {
+  setLanguage: (code: string) => void;
+}
+
+const LanguagePicker: React.FC<Props> = ({ setLanguage }) => {
+  const languages = [
+    { code: 'en', symbol: 'us' },
+    { code: 'br', symbol: 'br' },
+  ];
+
+  return (
+    <div data-test="component-language-picker">
+      {languages.map((language) => (
+        <span data-test="language-icon" key={language.code} onClick={() => setLanguage(language.code)}>
+          {language.symbol}
+        </span>
+      ))}
+    </div>
+  );
+};
+
+export default LanguagePicker;
